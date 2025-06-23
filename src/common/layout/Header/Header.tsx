dd26d872f37396edd565df1c12f3819c79ae0814
unknown <togatecomz.mj@gmail.com>
2025-06-23 18:00:50 +0900

    헤더 css

--- src/common/layout/Header/Header.tsx
+++ src/common/layout/Header/Header.tsx
@@ -1,11 +1,18 @@
 import styles from "./Header.module.scss";
 import userIcon from "../../../assets/images/user-icon.svg";
+// import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <img src={userIcon} alt="User Icon" className={styles.userIcon} />
-      <strong>홍길동</strong> 님
+      {/* <Link to="/"> */}
+      <div className={styles.userInfo}>
+        <div className={styles.imageWrapper}>
+          <img src={userIcon} alt="User Icon" />
+        </div>
+        {/* </Link> */}
+        <strong>홍길동</strong> 님
+      </div>
     </div>
   );
 }

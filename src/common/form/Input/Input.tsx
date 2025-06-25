ef21316d8360c07bef2885947cf5b13c7e89f62d
unknown <togatecomz.mj@gmail.com>
2025-06-25 10:56:40 +0900

    table, input 컴포넌트

---
+++ src/common/form/Input/Input.tsx
@@ -0,0 +1,28 @@
+import styles from "./Input.module.scss";
+type InputProps = {
+  type: "text" | "date" | "content";
+  placeholder?: string;
+  value?: string;
+  onChange?: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+};
+
+const Input = ({ type, placeholder, value, onChange }: InputProps) => {
+  if (type === "content")
+    return (
+      <textarea className={styles.contentInput} placeholder={placeholder} />
+    );
+
+  return (
+    <input
+      className={styles.textInput}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Input;
--- src/common/layout/Table/Table.tsx
+++ src/common/layout/Table/Table.tsx
@@ -1,16 +1,64 @@
+import Input from "../../form/Input/Input";
 import styles from "./Table.module.scss";
+
+const TABLE_EXAMPLE = {
+  headers: [
+    {
+      text: "수정자",
+      value: "modifier",
+    },
+    {
+      text: "수정일",
+      value: "date",
+    },
+    {
+      text: "제목",
+      value: "title",
+    },
+    {
+      text: "게시일",
+      value: "postDate",
+    },
+    {
+      text: "내용",
+      value: "content",
+    },
+  ],
+  listItems: [
+    {
+      modifier: "홍길동",
+      date: "2025.06.25",
+      title: "textInput",
+    },
+  ],
+};
 
 function Table() {
   return (
     <div className={styles.tableContainer}>
-      <h1>팝업관리</h1>
+      <h1>계정관리</h1>
       <div className={styles.tableWrapper}>
-        <h2>팝업 등록</h2>
+        <h2>게시판수정</h2>
         <table className={styles.table}>
-          <thead>
-            <tr></tr>
-          </thead>
-          <tbody></tbody>
+          <tbody>
+            {TABLE_EXAMPLE.headers.map((i) => (
+              <tr>
+                <th>{i.text}</th>
+                <td>
+                  {TABLE_EXAMPLE.listItems.map((j) => {
+                    if (i.value === "modifier" || i.value === "date")
+                      return <>{j[i.value]}</>;
+                    if (i.value === "title") return <Input type="text" />;
+                    if (i.value === "postDate") return <Input type="date" />;
+                    if (i.value === "content")
+                      return (
+                        <Input type="content" placeholder="텍스트 입력하세용" />
+                      );
+                  })}
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>

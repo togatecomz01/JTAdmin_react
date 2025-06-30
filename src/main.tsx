881cb3042c8d04d22d766c80d906db9ee17ef37b
choijisoon1 <choijisoon1@gmail.com>
2025-06-30 09:25:38 +0900

    vite 설정 수정, 해쉬라우터 변경

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom';
+import { /* BrowserRouter , */HashRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
